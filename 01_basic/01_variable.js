3f9ebb6b63142fe0d0f8cb6b7565e5bcdd486b6b
Aurank16 <mayankkrt123@gmail.com>
2024-11-13 01:37:24 +0530

    container config

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,22 @@
+const accountId =  45637
+let  accountEmail ="maynkk@jjj"
+var acccountCity ="jaipur"
+accountPassword = 76767
+/*
+In js without declaring the variable also we can create a memory but is 
+not good to use.
+*/
+
+// accountId = 7878//not allowed if once const is initialized
+accountEmail="hvvv@gmzil.com"
+acccountCity="varanasi"
+accountPassword=88888
+console.log(accountId)
+
+/*
+Prefered to use let insted of var
+coz of issue in block scope(once change value will change the each value 
+where it used againa nd again) and fuctional scope.
+*/
+
+ console.table([acccountCity,accountEmail,accountId,accountPassword])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"// it is syntex which shows or treat js code as newer version.
+
+// alert(3+3) // we are using node.js not browser'
+
+console.log(3+3);console.log("hello") // this is ryt bt not in not standerd way.
+
+let name ="mayank"
+let age = 23
+let isLoggedIn= false // there all are ex of datatype
+
+// some datatype are:-
+// number => 2^53(range) 
+// bigint=>(if thr number is too large then it get use bt normally not)
+// string=> ""
+// boolean=> true/false
+// null=> a standalone value
+// undefine=> standalone bt it is defined bt value is not assigned
+// symbol=> to find uniqness
+
+console.log(typeof null)/* typeof used to define type of datatype 
+use in console.log */
+// null is an object
+
+console.log(typeof undefined)// o/p:- undefine bt in null it is obj.
+
+
+
+
+ 
