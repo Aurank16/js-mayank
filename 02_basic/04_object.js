b41cdb8e27b4212d30749a11cb9563626496ab1e
Aurank16 <mayankkrt123@gmail.com>
2024-11-28 01:15:24 +0530

    obj & array

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -6,29 +6,29 @@ const tinderUser = {};
 tinderUser.id = "ma";
 tinderUser.rollNo = 23;
 
-//console.log(tinderUser)
+console.log(tinderUser)
 
-// const object = { // object in object
-//   object1: {
-//     firstName: "mayank",
-//     rollNo: 34,
-//     object2: {
-//       object3: {
-//         subject: "mathematics",
-//         object4: {
-//           loggedIn: true,
-//         },
-//       },
-//     },
-//   },
-// };
-//console.log(object.object1);
+const object = { // object in object
+  object1: {
+    firstName: "mayank",
+    rollNo: 34,
+    object2: {
+      object3: {
+        subject: "mathematics",
+        object4: {
+          loggedIn: true,
+        },
+      },
+    },
+  },
+};
+console.log(object.object1);
 
 const obj1 = { 2: "mayank", 3: "shivaka" };
 const obj2 = { 5: "venkatesh", 6: "kusha" };
 
-//const obj3={...obj1,...obj2} // both used to combine the obj
-// also we use const obj3 = object.assign(obj1,obj2)
+const obj3={...obj1,...obj2} // both used to combine the obj
+//also we use * const obj3 = object.assign(obj1,obj2)//assign
 
 const user1 = [
   //object is array
