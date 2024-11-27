57d2e8ebca9ea80efba04e3e78ed65d385da428a
Aurank16 <mayankkrt123@gmail.com>
2024-11-28 01:13:30 +0530

    obj & array

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -1,12 +1,12 @@
-// // // constructor or singleton object
+// // constructor or singleton object
 
-//  const user = new Object // initializing obj via constructor
+const user = new Object(); // initializing obj via constructor
 
-//  const tinderUser = {}
-//  tinderUser.id = "ma"
-//  tinderUser.rollNo=23
+const tinderUser = {};
+tinderUser.id = "ma";
+tinderUser.rollNo = 23;
 
-//  console.log(tinderUser)
+//console.log(tinderUser)
 
 // const object = { // object in object
 //   object1: {
@@ -22,15 +22,28 @@
 //     },
 //   },
 // };
-// console.log(object.object1);
+//console.log(object.object1);
 
-const obj1={2:"mayank",3:"shivaka"}
-const obj2={5:"venkatesh",6:"kusha"}
+const obj1 = { 2: "mayank", 3: "shivaka" };
+const obj2 = { 5: "venkatesh", 6: "kusha" };
 
-const obj3 = Object.assign({},obj1,obj2)//.assign used combine 
+//const obj3={...obj1,...obj2} // both used to combine the obj
+// also we use const obj3 = object.assign(obj1,obj2)
 
-console.log(obj3)
+const user1 = [
+  //object is array
+  {},
+  {
+    email: "mau@22343",
+  },
+  {
+    rollNo2:45,
+  },
+];
+console.log(user1[2].rollNo2)
 
+//**********************************************/
 
-
-
+console.log(tinderUser)
+console.log(Object.keys(tinderUser)); //print all keys value
+console.log(Object.values(tinderUser)); // print all values
