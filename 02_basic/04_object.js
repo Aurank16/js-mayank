bbedc447dd63ca04d5d9d23612d280d697884ad0
Aurank16 <mayankkrt123@gmail.com>
2024-11-28 00:18:42 +0530

    obj & array

---
+++ 02_basic/04_object.js
@@ -0,0 +1,36 @@
+// // // constructor or singleton object
+
+//  const user = new Object // initializing obj via constructor
+
+//  const tinderUser = {}
+//  tinderUser.id = "ma"
+//  tinderUser.rollNo=23
+
+//  console.log(tinderUser)
+
+// const object = { // object in object
+//   object1: {
+//     firstName: "mayank",
+//     rollNo: 34,
+//     object2: {
+//       object3: {
+//         subject: "mathematics",
+//         object4: {
+//           loggedIn: true,
+//         },
+//       },
+//     },
+//   },
+// };
+// console.log(object.object1);
+
+const obj1={2:"mayank",3:"shivaka"}
+const obj2={5:"venkatesh",6:"kusha"}
+
+const obj3 = Object.assign({},obj1,obj2)//.assign used combine 
+
+console.log(obj3)
+
+
+
+
